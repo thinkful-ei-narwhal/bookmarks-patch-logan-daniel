9bb8be32b1b25a7feb608380f96b91484cd430e0
nevelson <nevelson92@gmail.com>
2020-05-07 14:52:31 -0400

    Added tests

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,98 +1,103 @@
 /* eslint-disable quotes */
+require('dotenv').config();
 const app = require("../src/app");
 let store = require("../src/store");
+const knex = require("knex");
+const testFixture = require('./testfixture');
 
 describe("Bookmark endpoints", () => {
-  let bookmarksCopy;
+  let testDB;
+  let testObjectSeed = testFixture.getFixture();
   const authTokenTest = "Bearer my-secret";
-  beforeEach("copy the bookmarks", () => {
-    bookmarksCopy = store;
+
+  before(() => {
+    console.log(process.env.DB_TEST_URL);
+    testDB = knex({
+      client: 'pg',
+      connection: process.env.DB_TEST_URL
+    });
   });
+  before(() => app.set('db', testDB));
+  before(() => testDB('bookmarks').truncate());
+  after(() => testDB('bookmarks').truncate());
+  after(() => testDB.destroy());
 
-  afterEach("restore the bookmarks", () => {
-    store = bookmarksCopy;
-  });
 
-  // it('should return 200 "Hello world!"', () => {
-  //   return supertest(app)
-  //     .get("/")
-
-  //     .expect(200, { message: "Hello world!" });
-  // });
-
-  //positive tests
-  //200 on the three endpoints
-  describe("GET all bookmarks", () => {
+  context("Database has seed", () => {
+    beforeEach(() => testDB('bookmarks').insert(testObjectSeed));
+    afterEach(() => testDB('bookmarks').truncate());
     describe("GET all bookmarks happy path", () => {
-      it("gets the bookmarks from the store", () => {
+      it("Gets the bookmarks", () => {
         return supertest(app)
           .get("/bookmarks")
           .set("Authorization", authTokenTest)
-          .expect(200, store);
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.eql(testObjectSeed);
+          });
       });
 
       it("should get a particular book by ID from store", () => {
+        const idForTest = 1;
         return supertest(app)
-          .get("/bookmarks/0123")
+          .get(`/bookmarks/${idForTest}`)
           .set("Authorization", authTokenTest)
-          .expect(201, store[0]);
-      });
-
-      it("posts a bookmark to the store", () => {
-        const postValues = {
-          title: "test-title",
-          url: "http://some.thing.com",
-          rating: "1",
-        };
-        console.log(postValues);
-        return supertest(app)
-          .post("/bookmarks")
-          .send(postValues)
-          .set("Authorization", authTokenTest)
-          .expect(201)
-          .expect((res) => {
-            expect(res.body.title).to.equal(postValues.title);
-            expect(res.body.url).to.equal(postValues.url);
-          });
+          .expect(201, testObjectSeed[0]);
       });
     });
 
-    describe("GET all bookmarks unhappy path", () => {
-      it("should delete the bookmark specified by id", () => {
+    context("Database is empty", () => {
+      beforeEach(() => testDB('bookmarks').truncate());
+
+      it("Gets the bookmarks", () => {
         return supertest(app)
-          .delete("/bookmarks/0123")
+          .get("/bookmarks")
           .set("Authorization", authTokenTest)
-          .expect(201)
-          .expect(store.filter((x) => x.id == "0123"))
-          .to.equal("undefined");
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.eql([]);
+          });
+      });
+
+      it("should get a particular book by ID from store", () => {
+        const idForTest = 1;
+        return supertest(app)
+          .get(`/bookmarks/${idForTest}`)
+          .set("Authorization", authTokenTest)
+          .expect(404);
       });
     });
   });
-
-  //  .delete((req, res) => {
-  //   const { id } = req.params;
-  //   let delBm = data.findIndex((bm) => bm.id === id);
-
-  //   if (delBm === -1) {
-  //     logger.error(`Failed to delete : Bookmark ${delBm.title} `);
-  //     return res.status(404).send(`Bookmark with id ${id} was not found`);
-  //   }
-
-  //   data.splice(delBm, 1);
-  //   logger.info(
-  //     `Successful delete : Bookmark ${delBm.title} was deleted with id: ${delBm.id}`
-  //   );
-  //   res.status(201).send(`Bookmark with id ${id} was deleted`);
-  // });
-
-  // describe();
-
-  //negative tests
-  //400 on all the expected endpoints
-
-  //proper token passes
-  //junk token fails
-
-  //missing bearer fails
-  //having bearer passes
 });
+
+
+//     it("posts a bookmark to the store", () => {
+//       const postValues = {
+//         title: "test-title",
+//         url: "http://some.thing.com",
+//         rating: "1",
+//       };
+//       return supertest(app)
+//         .post("/bookmarks")
+//         .send(postValues)
+//         .set("Authorization", authTokenTest)
+//         .expect(201)
+//         .expect((res) => {
+//           expect(res.body.title).to.equal(postValues.title);
+//           expect(res.body.url).to.equal(postValues.url);
+//         });
+//     });
+//   });
+
+//   describe("GET all bookmarks unhappy path", () => {
+//     it("should delete the bookmark specified by id", () => {
+//       return supertest(app)
+//         .delete("/bookmarks/0123")
+//         .set("Authorization", authTokenTest)
+//         .expect(201)
+//         .expect(store.filter((x) => x.id == "0123"))
+//         .to.equal("undefined");
+//     });
+//   });
+// });
+
